bfb3be9458b98ebe07d85c8e171eda30d41f5e6b
ptrk <paw.toporkov@gmail.com>
2020-01-24 22:00:28 +0300

    first commit

---
+++ SmartGame/SmartGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartGame
+//
+//  Created by Paw.toporkov on 24/01/2020.
+//  Copyright © 2020 TemaPasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
